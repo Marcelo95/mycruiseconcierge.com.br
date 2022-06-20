97dec39e440898aeb41b9b108f8303e0d4dd063a
Marcelo Vieira <marcelovieira1995@gmail.com>
2022-06-20 19:10:15 -0300

    ajust script

--- tema/single-pre-embarque.php
+++ tema/single-pre-embarque.php
@@ -1466,39 +1466,36 @@ get_header(); ?>
 			//data-futura-embarque
 			//data-futura-desembarque
 
-			$(function() {
-				var dateFormat = "mm/dd/yy",
-					from = $(".data-futura-embarque")
-					.datepicker({
-						language: "pt-BR",
-						autoclose: true,
-						changeMonth: true,
-						minDate: 0
-					})
-					.on("change", function() {
-						to.datepicker("option", "minDate", getDate(this));
-					}),
-					to = $(".data-futura-desembarque").datepicker({
-						language: "pt-BR",
-						autoclose: true,
-						changeMonth: true,
-					})
-					.on("change", function() {
-						from.datepicker("option", "maxDate", getDate(this));
-					});
+			var dateFormat = "mm/dd/yy",
+				from = $(".data-futura-embarque")
+				.datepicker({
+					language: "pt-BR",
+					autoclose: true,
+					changeMonth: true,
+					minDate: 0
+				})
+				.on("change", function() {
+					to.datepicker("option", "minDate", getDate(this));
+				}),
+				to = $(".data-futura-desembarque").datepicker({
+					language: "pt-BR",
+					autoclose: true,
+					changeMonth: true,
+				})
+				.on("change", function() {
+					from.datepicker("option", "maxDate", getDate(this));
+				});
 
-				function getDate(element) {
-					var date;
-					try {
-						date = $.datepicker.parseDate(dateFormat, element.value);
-					} catch (error) {
-						date = null;
-					}
-
-					return date;
+			function getDate(element) {
+				var date;
+				try {
+					date = $.datepicker.parseDate(dateFormat, element.value);
+				} catch (error) {
+					date = null;
 				}
-			});
 
+				return date;
+			}
 
 
 			jQuery(".data-passada").datepicker({
